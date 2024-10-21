3e75cda6861d64f9de3e8bd1463ed71b07abd5d3
LuzPaty <luzpatyhr@gmail.com>
2024-10-21 09:29:54 -0600

    funcion move_food()

---
+++ snake.py
@@ -0,0 +1,88 @@
+"""Snake, classic arcade game.
+
+Exercises
+
+1. How do you make the snake faster or slower?
+2. How can you make the snake go around the edges?
+3. How would you move the food?
+4. Change the snake to respond to mouse clicks.
+"""
+
+from random import randrange
+from random import choice
+
+from turtle import *
+
+from freegames import square, vector
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+
+def change(x, y):
+    """Change snake direction."""
+    aim.x = x
+    aim.y = y
+
+
+def inside(head):
+    """Return True if head inside boundaries."""
+    return -200 < head.x < 190 and -200 < head.y < 190
+
+def move_food():
+    """Move food randomly one step without leaving the window."""
+    directions = [(10, 0), (-10, 0), (0, 10), (0, -10)]  # Derecha, Izquierda, Arriba, Abajo
+    dx, dy = choice(directions)  # Elegir una dirección al azar
+
+    # Updates coordinates
+    food.x += dx
+    food.y += dy
+
+    # Food window boundaries 
+    if not inside(food):
+        # Si se sale, revertir el movimiento
+        food.x -= dx
+        food.y -= dy
+
+def move():
+    """Move snake forward one segment."""
+    head = snake[-1].copy()
+    head.move(aim)
+
+    if not inside(head) or head in snake:
+        square(head.x, head.y, 9, 'red')
+        update()
+        return
+
+    snake.append(head)
+
+    if head == food:
+        print('Snake:', len(snake))
+        food.x = randrange(-15, 15) * 10
+        food.y = randrange(-15, 15) * 10
+    else:
+        snake.pop(0)
+
+    move_food()
+
+    clear()
+
+    for body in snake:
+        square(body.x, body.y, 9, 'black')
+
+    square(food.x, food.y, 9, 'green')
+    update()
+    ontimer(move, 100)
+
+
+setup(420, 420, 370, 0)
+hideturtle()
+tracer(False)
+listen()
+onkey(lambda: change(10, 0), 'Right')
+onkey(lambda: change(-10, 0), 'Left')
+onkey(lambda: change(0, 10), 'Up')
+onkey(lambda: change(0, -10), 'Down')
+move()
+done()
